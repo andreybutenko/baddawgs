d056da7fbb042ee8dbc1d33ed968c82eec6828e3
KairstenFay <kairsten.fay@gmail.com>
2018-10-21 11:35:57 -0700

    edited circle chart colors

--- website/js/donut/donut.js
+++ website/js/donut/donut.js
@@ -29,7 +29,9 @@ dataCore.onDataLoaded(function(data) {
                     style: {
                         fontWeight: 'bold',
                         color: 'white'
-                    }
+                    },
+                    crop: false,
+                    overflow: 'none'
                 },
                 startAngle: -90,
                 endAngle: 90,
@@ -39,25 +41,66 @@ dataCore.onDataLoaded(function(data) {
         },
         series: [{
             type: 'pie',
-            name: '% of Records',
+            name: 'Percent of Records',
             innerSize: '50%',
             data: [
-                ['Property & Trespass', data.filter(row => row['category'] == 'property-trespass').length],
-                ['Assault', data.filter(row => row['category'] == 'assault').length],
                 ['Rape', data.filter(row => row['category'] == 'rape').length],
-                ['Substance', data.filter(row => row['substance'] == 'other').length],
-                {
-                    name: 'Other',
-                    y: data.filter(row => row['category'] == 'other').length,
-                    dataLabels: {
-                        enabled: false
-                    }
-                }
+                ['Property & Trespass', data.filter(row => row['category'] == 'property-trespass').length],
+                ['Substance', data.filter(row => row['category'] == 'substance').length],
+                ['Assault', data.filter(row => row['category'] == 'assault').length],
+                ['Other', data.filter(row => row['category'] == 'other').length]
+
+
+                // {
+                //     name: 'Other',
+                //     y: data.filter(row => row['category'] == 'other').length,
+                //     dataLabels: {
+                //         enabled: false
+                //     }
+                // }
             ]
         }]
     });
 
 });
+
+Highcharts.theme = {
+    colors: ['#C0392B', '#2880B9', '#27AE60', '#D35401', '#999999'],
+    // chart: {
+    //     backgroundColor: {
+    //         linearGradient: [0, 0, 500, 500],
+    //         stops: [
+    //             [0, 'rgb(255, 255, 255)'],
+    //             [1, 'rgb(240, 240, 255)']
+    //         ]
+    //     },
+    // },
+    title: {
+        style: {
+            color: '#000',
+            font: 'bold 16px "Trebuchet MS", Verdana, sans-serif'
+        }
+    },
+    subtitle: {
+        style: {
+            color: '#666666',
+            font: 'bold 12px "Trebuchet MS", Verdana, sans-serif'
+        }
+    },
+
+    legend: {
+        itemStyle: {
+            font: '9pt Trebuchet MS, Verdana, sans-serif',
+            color: 'black'
+        },
+        itemHoverStyle:{
+            color: 'gray'
+        }
+    }
+};
+
+// Apply the theme
+Highcharts.setOptions(Highcharts.theme);
 
 /*
 temp1.map(row => {
