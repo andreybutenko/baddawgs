8c05469e8e55fdd5b59280a9e999c6eab4f4add2
caleb@linearity.io <caleb@linearity.io>
2018-10-21 11:37:39 -0700

    WE need binzzz

--- website/js/histogram/histogram.js
+++ website/js/histogram/histogram.js
@@ -5,49 +5,35 @@ var incidentDateArray;
 dataCore.onDataLoaded(function(data) {
     console.log("HI");
     var arr = data.map(row => {
-        var parsed = Date.parse(row['Reported Date and Time']);
-        return parsed.getTime()
+        return Date.parse(row['Reported Date and Time']).getTime()
     });
     incidentDateArray = arr;
 
     console.log(incidentDateArray);
 
-    Highcharts.chart('histogramtest', {
-        title: {
-            text: 'Highcharts Histogram'
+    Highcharts.chart({
+
+        chart: {
+            renderTo: 'histogramtest',
+            type: 'column'
         },
-        xAxis: [{
-            title: { text: 'Data' },
-            alignTicks: false
-        }, {
-            title: { text: 'Histogram' },
-            alignTicks: false,
-            opposite: true
-        }],
     
-        yAxis: [{
-            title: { text: 'Data' }
-        }, {
-            title: { text: 'Histogram' },
-            opposite: true
-        }],
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        
+        plotOptions: {
+            column: {
+                groupPadding: 0,
+                pointPadding: 0,
+                borderWidth: 0
+            }
+        },
     
         series: [{
-            name: 'Histogram',
-            type: 'histogram',
-            xAxis: 1,
-            yAxis: 1,
-            baseSeries: 's1',
-            zIndex: -1
-        }, {
-            name: 'Data',
-            type: 'scatter',
-            data: incidentDateArray,
-            id: 's1',
-            marker: {
-                radius: 1.5
-            }
+            data: incidentDateArray
         }]
+    
     });
 });
 
